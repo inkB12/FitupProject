646ad277749061b50d7be20d0318932221b994c2
inkB12 <ttinhbao@gmail.com>
2026-03-11 21:31:41 +0700

    [FEAT] Health

---
+++ FitupProject/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitupProject.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HealthController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok("ok");
+        }
+    }
+}
